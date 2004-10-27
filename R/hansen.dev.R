59e3e60f7defc3e14225d1a5b6efe2f637e5e458
Aaron A. King <king@tiem.utk.edu>
2004-10-27 00:00:00 +0000

    version 1.0-3

--- R/hansen.dev.R
+++ R/hansen.dev.R
@@ -2,6 +2,6 @@ hansen.dev <- function(n = 1, topology, times, regimes, alpha, sigma, theta) {
   pt <- parse.tree(topology,times,regimes);
   w <- weight.matrix(alpha, pt);
   v <- scaled.covariance.matrix(alpha, pt);
-  x <- rmvnorm(n, mu = as.vector(w %*% theta), Sigma = as.numeric(sigma^2)*v);
+  x <- rmvnorm(n, as.vector(w %*% theta), as.numeric(sigma^2)*v);
   return(data.frame(x))
 }
