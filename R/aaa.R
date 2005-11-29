320fa77ea5baf00e8a7d905c65c60329a2c10b02
Aaron A. King <kingaa@umich.edu>
2005-11-29 00:00:00 +0000

    version 1.1-2

--- R/aaa.R
+++ R/aaa.R
@@ -1,7 +1,7 @@
 # This file is part of the OUCH package.
-# Author: Aaron A. King <king at tiem dot utk dot edu>
+# Author: Aaron A. King <kingaa at umich dot edu>
 # It is distributed under the GNU Public License (see the file GPL
 # included)
 # The OUCH package is maintained at
-#          http://www.tiem.utk.edu/~king/ouch/
+#          http://tsuga.biology.lsa.umich.edu/ouch/
 #
